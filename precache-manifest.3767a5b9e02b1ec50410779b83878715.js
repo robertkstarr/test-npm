ae1f2099f1a554bfedcb245c0ae49763f81a8759
Robert Starr <rstarr@indeed.com>
2020-04-23 17:08:26 -0500

    Updates

---
+++ precache-manifest.3767a5b9e02b1ec50410779b83878715.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3a68574615076ea010e26d6d5f79a439",
+    "url": "/test-npm/index.html"
+  },
+  {
+    "revision": "fb7be202cf7f17c9188d",
+    "url": "/test-npm/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "b77abdd238a371979637",
+    "url": "/test-npm/static/js/2.fe082c35.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/test-npm/static/js/2.fe082c35.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fb7be202cf7f17c9188d",
+    "url": "/test-npm/static/js/main.41f9d00c.chunk.js"
+  },
+  {
+    "revision": "b12a241109e184eec4c2",
+    "url": "/test-npm/static/js/runtime-main.39307e14.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/test-npm/static/media/logo.5d5d9eef.svg"
+  }
+]);
