1785d1ecb6f9bd3d23a8c850d2fc20def7443cc6
Robert Starr <rstarr@indeed.com>
2020-04-23 17:44:27 -0500

    Created css elements in flex.

---
+++ src/Dice.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+// class Dice extends React.Component {
+//     render() {
+//         return <div className={Dice}>Test</div>
+//     }
+// }
+
+function Dice(props) {
+    return <div class={"Dice"}>Test</div>
+}
+
+export default Dice;
